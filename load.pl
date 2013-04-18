1c7582277f1172ceb8ff2767f2a931bdffcc3657
Eszter Fodor <chytos@gmail.com>
2013-04-18 17:48:06 +0200

    merge

--- load.pl
+++ load.pl
@@ -11,13 +11,9 @@ load:-
   source_file(load, ThisFile),
   file_directory_name(ThisFile, ThisDirectory),
   assert(user:file_search_path(project, ThisDirectory)),
-<<<<<<< HEAD
-  assert(user:file_search_path(generic, project('Generic'))),
-=======
   
   % This project uses the PGC library.
   assert(user:file_search_path(generic, project('PrologGenericsCollection'))),
->>>>>>> 1d3f6c78b61865593185bf1c6701a3b0247bf240
   
   % Use the swipl debug monitor for viewing debug messages.
   prolog_ide(debug_monitor),
