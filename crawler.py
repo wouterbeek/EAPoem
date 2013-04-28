5776e36623bb9241332ac24a1d210be0e7d81681
Eszter Fodor <chytos@gmail.com>
2013-04-28 17:42:21 +0200

    Crawler coming along pretty well

--- crawler.py
+++ crawler.py
@@ -1,37 +1,77 @@
 """
-This will eventually be a webcrawler to get poems from poemhunter.com
+This will eventually be a webcrawler to get poems from the web
 At this point this is only a minimalistic crawler since this will be my first crawler
 
 Author: Eszter Fodor
 """
 
+import sys
 import mechanize
 import MySQLdb
+import re
 from BeautifulSoup import BeautifulSoup
+from HTMLParser import HTMLParser
+
+"""
+Tags to replace in texts
+"""
+reps = ["<td>", "\t", "\r", "</td>"]
 
 
-br = mechanize.Browser()
-site = br.open("http://loremipsum.net/")
-html = site.read()
+def getPoem(html):
+	"""
+	Get the poem from the website
+	The structure of everywebsite is different which results in the fact that the line
+	"find = str(soup.body.table.contents[3].td)" must be changed for every website
+	"""
+	soup = BeautifulSoup(html)
 
-soup = BeautifulSoup(html)
-body = soup.body
-lorem = str(body.contents[1].contents[3].p)
-loremSplit = lorem.split("<p>")
-loremRight = loremSplit[1]
+	find = str(soup.body.table.contents[3].td)
+	poemSplit = find.split("<br>")
+	poem = strip_tags(poemSplit)
+	
+	return poem
+	
+def strip_tags(text):
+	"""
+	Strip the text from the tags
+	"""
+	for line in text:
+		for tag in reps:
+			if tag in line:
+				line = line.replace(tag, "")
+				if "<br />" in line:
+					line = line.replace("<br />", "\n")
+	return line		
+	
 
-db = MySQLdb.connect(host = "localhost", user = "root", passwd = "", db = "test")
-cur = db.cursor() 
+def databaseFunction(poem, author, style):
+	"""
+	Insert the poem, the author and the style in the database
+	"""
+	db = MySQLdb.connect(host = "localhost", user = "root", passwd = "", db = "test")
+	cur = db.cursor() 
 
-if cur: 
-	cur.execute("INSERT INTO poemhunter VALUES (4, %s, 'test1', 'test2')", loremRight)
-	db.commit()
-else:
-	print "Something went wrong"
+	if cur: 
+		cur.execute("INSERT INTO poemhunter VALUES (NULL, %s, %s, %s)", [poem, author, style])
+		db.commit()
+	else:
+		print "Something went wrong"
 
-dbClose = db.close()
+	dbClose = db.close()
 
+def main():
+	"""
+	Program entry point
+	"""
+	br = mechanize.Browser()
+	site = br.open("http://poetry.eserver.org/sonnets/001.html")
+	html = site.read()
+	poems = getPoem(html)
+	
+	databaseFunction(poems, "William Shakespeare", "sonnet")
 
+if __name__ == '__main__': main()
 
 
 
