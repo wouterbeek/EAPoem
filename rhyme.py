c35059f2a8c39b616c79ff60be5731c57a81309d
Eszter Fodor <chytos@gmail.com>
2013-05-25 00:33:57 +0200

    Dictionary almost finished

--- rhyme.py
+++ rhyme.py
@@ -10,29 +10,50 @@ Version: 05/2013
 import re
 import os
 import sys
+import itertools
 
-def loadFile(filename):
+
+def loadFile(english, output):
 	"""
 	Read the file and split between the words and their phonetics
 	"""
-	file = open(filename, 'r')
-	file.split('\n')
+	phonetics = open(english, 'r')
+	out = open(output, 'w')
+	buffer = phonetics.read().split('\n')
+	return (buffer, out)
+		
 
-def getRhymes(phonetics):
-	phonSplit = phonetics.split('-')
-	phonReverse = phonSplit.reverse()
-	print phonReverse
-	
-	
+def getRhymes(loaded,out):
+	"""
+	Get the phonetics of every word and reverse it
+	Create dictionary with every word and the ending phonetics
+	"""
+	for line in loaded:
+		if line == '':
+			break
+		else:
+			(word, phon) = line.split('\\')
+			phonSplit = phon.split('-')
+			phonReverse = phonSplit[::-1]
+			line = dict([(word, phonReverse[0])])
+			for key in line.keys():
+				rhyme = []
+				rhyme.append(key)
+				keyVal = line[key]
+				words = [key for key, val in line.iteritems() if val == keyVal]
+				rhyme.append(words)
+			out.write(str(rhyme)) 
+			out.write("\n")
+
 	
 def main(args):
 	"""
 	Program entry point
 	Argument: filename
 	"""
-	load = loadFile(args[1])
-	#for line in load:
-		#print line
+	(load,out) = loadFile(args[1], args[2])
+	getRhymes(load,out)
+	
 	
 	
 if __name__ == '__main__':
