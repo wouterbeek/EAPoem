192e0cdca9d3e90a84b41426ad13fe6b85b605b5
Eszter Fodor <chytos@gmail.com>
2013-05-25 17:20:26 +0200

    Rhyme dictionary ready

--- rhyme.py
+++ rhyme.py
@@ -11,6 +11,7 @@ import re
 import os
 import sys
 import itertools
+import time
 
 
 def loadFile(english, output):
@@ -28,22 +29,36 @@ def getRhymes(loaded,out):
 	Get the phonetics of every word and reverse it
 	Create dictionary with every word and the ending phonetics
 	"""
+	lines = dict([])
+	# For every line in file
 	for line in loaded:
 		if line == '':
 			break
 		else:
+			# Get word and phonetics
 			(word, phon) = line.split('\\')
 			phonSplit = phon.split('-')
 			phonReverse = phonSplit[::-1]
-			line = dict([(word, phonReverse[0])])
-			for key in line.keys():
-				rhyme = []
-				rhyme.append(key)
-				keyVal = line[key]
-				words = [key for key, val in line.iteritems() if val == keyVal]
-				rhyme.append(words)
-			out.write(str(rhyme)) 
-			out.write("\n")
+			# Add word and last part of phonetics to dictionary
+			lines.update({word:phonReverse[0]})
+	# For every word in dictionary
+	for key in lines.keys():
+		rhyme = []
+		words = []
+		# Add the word to list
+		rhyme.append(key)
+		keyVal = lines[key]
+		# For every other word in the dictionary
+		for keys, values in lines.items():
+			# If it's value is the same as of the word
+			if values == keyVal:
+				# Add the word to list of words
+				words.append(keys)
+	
+		# rhyme = list of list with [word, [rhyming words]]	
+		rhyme.append(words)
+		out.write(str(rhyme)) 
+		out.write("\n")
 
 	
 def main(args):
@@ -51,8 +66,12 @@ def main(args):
 	Program entry point
 	Argument: filename
 	"""
+	begin = time.time()
 	(load,out) = loadFile(args[1], args[2])
 	getRhymes(load,out)
+	
+	end = time.time()-begin
+	rint 'Time taken: %d min and %d sec' % (end/60, end%60)
 	
 	
 	
